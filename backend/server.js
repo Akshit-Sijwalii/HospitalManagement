476bd77c8c6adc5780c3fb63f78f88f187165dea
Manish Joshi <manishjoshi182003@gmail.com>
2025-05-10 18:02:11 +0530

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,12 @@ connectCloudinary();
 updateAppointmentStatus();
 
 //---------------------------------------- Middleware ----------------------------------------
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // Or set to your frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 
 //---------------------------------------- API Endpoints ----------------------------------------
